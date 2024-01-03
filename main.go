29e415c77a0311bab4fd7a462f591f50ce0ff81f
Gabriel Morais <gmdeveloper@gmail.com>
2024-01-02 21:15:59 -0300

    Iniciando api rest go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"api-rest-go/models"
+	"api-rest-go/routes"
+	"fmt"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+		{Id: 3, Nome: "Nome 3", Historia: "Historia 3"},
+		{Id: 4, Nome: "Nome 4", Historia: "Historia 4"},
+	}
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
