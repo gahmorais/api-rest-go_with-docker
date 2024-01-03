0c77c2664fbbea36dea4793ba48fceebda0d319a
Gabriel Morais <gmdeveloper@gmail.com>
2024-01-02 21:44:10 -0300

    Adicionando banco de dados

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
+	"api-rest-go/database"
 	"api-rest-go/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -15,16 +15,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	database.Db.Find(&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+	var p models.Personalidade
+	database.Db.First(&p, id)
+	json.NewEncoder(w).Encode(p)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api-rest-go/database"
 	"api-rest-go/models"
 	"api-rest-go/routes"
 	"fmt"
@@ -13,6 +14,7 @@ func main() {
 		{Id: 3, Nome: "Nome 3", Historia: "Historia 3"},
 		{Id: 4, Nome: "Nome 4", Historia: "Historia 4"},
 	}
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
