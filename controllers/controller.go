c361351cbd5d32deab368601dbeb68e133cb8289
Gabriel Morais <gabriel@backupautomacao.com.br>
2024-01-04 13:15:26 -0300

    Adicionado politica de CORS e frontend em react. Projeto finalizado

--- controllers/controller.go
+++ controllers/controller.go
@@ -27,3 +27,29 @@ func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	database.Db.First(&p, id)
 	json.NewEncoder(w).Encode(p)
 }
+
+func CriaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.Db.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.Db.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.Db.First(&personalidade, id)
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.Db.Save(&personalidade)
+	json.NewEncoder(w).Encode(&personalidade)
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,17 +2,22 @@ package routes
 
 import (
 	"api-rest-go/controllers"
+	"api-rest-go/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
-
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalidades", controllers.CriaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
